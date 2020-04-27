d225536aafb4eb8e955e006c051d8470716a5b4e
codedcosmos <coded@noreply.com>
2020-04-27 12:13:16 +1000

    1.4 jar

--- src/codedcosmos/cometbot/core/CometBot.java
+++ src/codedcosmos/cometbot/core/CometBot.java
@@ -35,7 +35,7 @@ import java.util.Random;
 public class CometBot {
 
 	// Version
-	public static final String VERSION = "1.3";
+	public static final String VERSION = "1.4";
 
 	// Guilds
 	public static GuildHandler<CometGuildContext> guilds;
